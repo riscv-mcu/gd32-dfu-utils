86eb294b8c81cfd989f21c0519749e08a7cbcbdb
Hans Petter Selasky <hps@bitfrost.no>
2013-04-10 17:04:37 +0200

    The CRC function and table do not need to be global
    Signed-off-by: Hans Petter Selasky <hps@bitfrost.no>

--- src/dfu_file.c
+++ src/dfu_file.c
@@ -29,7 +29,7 @@
 
 #define DFU_SUFFIX_LENGTH 16
 
-unsigned long crc32_table[] = {
+static const unsigned long crc32_table[] = {
     0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
     0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
     0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
@@ -74,7 +74,7 @@ unsigned long crc32_table[] = {
     0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
     0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d};
 
-uint32_t crc32_byte(uint32_t accum, uint8_t delta)
+static uint32_t crc32_byte(uint32_t accum, uint8_t delta)
 {
         return crc32_table[(accum ^ delta) & 0xff] ^ (accum >> 8);
 }
