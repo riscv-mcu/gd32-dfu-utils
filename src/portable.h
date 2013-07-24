2f7ef5bdcaca4bd33abc05fa0ff263d873d3f501
Tormod Volden <debian.tormod@gmail.com>
2013-07-24 21:53:45 +0200

    portable.h: Add err/errx/warn/warnx macros if not system provided
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- src/portable.h
+++ src/portable.h
@@ -21,4 +21,32 @@
 # error "Can't get no sleep! Please report"
 #endif /* HAVE_NANOSLEEP */
 
+#ifdef HAVE_ERR
+# include <err.h>
+#else
+# include <errno.h>
+# include <string.h>
+# define warnx(...) do {\
+    fprintf(stderr, __VA_ARGS__);\
+    fprintf(stderr, "\n"); } while (0)
+# define errx(eval, ...) do {\
+    warnx(__VA_ARGS__);\
+    exit(eval); } while (0)
+# define warn(...) do {\
+    fprintf(stderr, "%s: ", strerror(errno));\
+    warnx(__VA_ARGS__); } while (0)
+# define err(eval, ...) do {\
+    warn(__VA_ARGS__);\
+    exit(eval); } while (0)
+#endif /* HAVE_ERR */
+
+#ifdef HAVE_SYSEXITS_H
+# include <sysexits.h>
+#else
+# define EX_OK		0	/* successful termination */
+# define EX_USAGE	64	/* command line usage error */
+# define EX_SOFTWARE	70	/* internal software error */
+# define EX_IOERR	74	/* input/output error */
+#endif /* HAVE_SYSEXITS_H */
+
 #endif /* PORTABLE_H */
